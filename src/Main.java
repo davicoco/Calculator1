c784282551daa6be3119559d4dc62b51df7912e4
David <david_izqpue@hotmail.com>
2024-09-20 12:32:49 +0200

    la till int hejhej

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+            int hejhej;
             int variable1;
             int variable2;
             int result;
